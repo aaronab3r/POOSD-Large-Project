613e8f26f8e1483782a2f7b9abd6c09fe124524b
Josh <jawshs08@gmail.com>
2025-03-05 22:17:49 -0500

    im so tired man

--- exampleDoc/server.js
+++ exampleDoc/server.js
@@ -1,119 +1,12 @@
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config;
+const url = process.env.MONGODB_URI;
+const dbName = 'test';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-const MongoClient = require('mongodb').MongoClient;
-// There might be an issue with the url
-const url = 'RESTRICTED'; // this cannot be put into the github
-
-const client = new MongoClient(url);
-client.connect();
-
 const app = express();
-
-var cardList = 
-[
-  'Roy Campanella',
-  'Paul Molitor',
-  'Tony Gwynn',
-  'Dennis Eckersley',
-  'Reggie Jackson',
-  'Gaylord Perry',
-  'Buck Leonard',
-  'Rollie Fingers',
-  'Charlie Gehringer',
-  'Wade Boggs',
-  'Carl Hubbell',
-  'Dave Winfield',
-  'Jackie Robinson',
-  'Ken Griffey, Jr.',
-  'Al Simmons',
-  'Chuck Klein',
-  'Mel Ott',
-  'Mark McGwire',
-  'Nolan Ryan',
-  'Ralph Kiner',
-  'Yogi Berra',
-  'Goose Goslin',
-  'Greg Maddux',
-  'Frankie Frisch',
-  'Ernie Banks',
-  'Ozzie Smith',
-  'Hank Greenberg',
-  'Kirby Puckett',
-  'Bob Feller',
-  'Dizzy Dean',
-  'Joe Jackson',
-  'Sam Crawford',
-  'Barry Bonds',
-  'Duke Snider',
-  'George Sisler',
-  'Ed Walsh',
-  'Tom Seaver',
-  'Willie Stargell',
-  'Bob Gibson',
-  'Brooks Robinson',
-  'Steve Carlton',
-  'Joe Medwick',
-  'Nap Lajoie',
-  'Cal Ripken, Jr.',
-  'Mike Schmidt',
-  'Eddie Murray',
-  'Tris Speaker',
-  'Al Kaline',
-  'Sandy Koufax',
-  'Willie Keeler',
-  'Pete Rose',
-  'Robin Roberts',
-  'Eddie Collins',
-  'Lefty Gomez',
-  'Lefty Grove',
-  'Carl Yastrzemski',
-  'Frank Robinson',
-  'Juan Marichal',
-  'Warren Spahn',
-  'Pie Traynor',
-  'Roberto Clemente',
-  'Harmon Killebrew',
-  'Satchel Paige',
-  'Eddie Plank',
-  'Josh Gibson',
-  'Oscar Charleston',
-  'Mickey Mantle',
-  'Cool Papa Bell',
-  'Johnny Bench',
-  'Mickey Cochrane',
-  'Jimmie Foxx',
-  'Jim Palmer',
-  'Cy Young',
-  'Eddie Mathews',
-  'Honus Wagner',
-  'Paul Waner',
-  'Grover Alexander',
-  'Rod Carew',
-  'Joe DiMaggio',
-  'Joe Morgan',
-  'Stan Musial',
-  'Bill Terry',
-  'Rogers Hornsby',
-  'Lou Brock',
-  'Ted Williams',
-  'Bill Dickey',
-  'Christy Mathewson',
-  'Willie McCovey',
-  'Lou Gehrig',
-  'George Brett',
-  'Hank Aaron',
-  'Harry Heilmann',
-  'Walter Johnson',
-  'Roger Clemens',
-  'Ty Cobb',
-  'Whitey Ford',
-  'Willie Mays',
-  'Rickey Henderson',
-  'Babe Ruth'
-];
-
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -121,7 +14,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) =>
 {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader (
+	res.setHeader(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 	);
@@ -134,83 +27,75 @@ app.use((req, res, next) =>
 
 app.post('/api/addcard', async (req, res, next) =>
 {
-  // incoming: userId, color
-  // outgoing: error
-
-  const { userId, card } = req.body;
-
-  const newCard = {Card:card, UserId:userId};
+  // incoming: userId, color
+  // outgoing: error
+  
+  const client = new MongoClient(url);
+  client.connect();
+  
+  const { userId, card } = req.body;
+  const newCard = {Card:card,UserId:userId};
   var error = '';
-
   try
   {
-	  const db = client.db();
-	  const result = db.collection('Cards').insertOne(newCard);
+    const db = client.db(dbName);
+    const result = db.collection('Cards').insertOne(newCard);
   }
   catch(e)
   {
-	  error = e.toString();
+    error = e.toString();
   }
- 
   cardList.push( card );
-
-  var ret = {error: error};
-  res.status(200).json(ret);
+  var ret = { error: error };
+  res.status(200).tsxon(ret);
 });
 
-app.post('/api/login', async (req, res, next) => 
+app.post('/api/login', async (req, res, next) =>
 {
-  // incoming: login, password
-  // outgoing: id, firstName, lastName, error
-
-  var error = '';
-
-  const { login, password } = req.body;
-
-  const db = client.db();
-  const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
-
-  var id = -1;
-  var fn = '';
-  var ln = '';
-
-  if ( results.length > 0) 
+  // incoming: login, password
+  // outgoing: id, firstName, lastName, error
+  
+  const client = new MongoClient(url);
+  client.connect();
+  
+  var error = '';
+  const { login, password } = req.body;
+  const db = client.db(dbName);
+  const results = await
+  db.collection('Users').find({Login:login,Password:password}).toArray();
+  var id = -1;
+  var fn = '';
+  var ln = '';
+  if( results.length > 0 )
   {
     id = results[0].UserId;
     fn = results[0].FirstName;
-    ln = results[0].LastName; 
-  } 
-  
+     ln = results[0].LastName;
+  }
   var ret = { id:id, firstName:fn, lastName:ln, error:''};
-  res.status(200).json(ret);
+  res.status(200).tsxon(ret);
 });
 
-app.post('/api/searchcards', async (req, res, next) => 
+app.post('/api/searchcards', async (req, res, next) =>
 {
-  // incoming: userId, search
-  // outgoing: results[], error
-
-  var error = '';
-
-  const { userId, search } = req.body;
-  var _search = search.toLowerCase().trim();
-
-  const db = client.db();
+  // incoming: userId, search
+  // outgoing: results[], error
+  
+  const client = new MongoClient(url);
+  client.connect();
+  
+  var error = '';
+  const { userId, search } = req.body;
+  var _search = search.trim();
+  const db = client.db(dbName);
   const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'r'}}).toArray();
-
-  var _ret = [];
-
-  for( var i=0; i<results.length; i++ )
-  {
-    var lowerFromList = results[i].toLocaleLowerCase();
-    if( lowerFromList.indexOf( _search ) >= 0 )
-    {
-      _ret.push( results[i] );
-    }
-  }
-
-  var ret = {results:_ret, error:error};
-  res.status(200).json(ret);
-});165.227.65.153
+  var _ret = [];
+  for( var i=0; i<results.length; i++ )
+  {
+    _ret.push( results[i].Card );
+  }
+  var ret = {results:_ret, error:error};
+  res.status(200).tsxon(ret);
+});
 
 app.listen(5000); // start Node + Express server on port 5000
