203375931d3e74ef6aedac5d13895e49f3e53e71
Josh <jawshs08@gmail.com>
2025-03-13 12:05:24 -0400

    update App.tsx (src App)

--- cards/frontend/src/App.tsx
+++ cards/frontend/src/App.tsx
@@ -1,15 +1,16 @@
 //import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
-import LoginPage from './pages/LoginPage';
+//import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
+import AppPage from './pages/App';
 
 function App() {
   return (
   <Router >
     <Switch>
       <Route path="/" exact>
-        <LoginPage />
+        <AppPage />
       </Route>
       <Route path="/cards" exact>
         <CardPage />
